baf1309e64c1fb3c740ff80498789f093216ba89
WCFX <wcfx.dev@gmail.com>
2020-07-26 22:59:40 -0300

    Criando a criacao de users

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,72 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { 
+  Container,
+  Title,
+  BackToLogin,
+  BackToLoginText
+} from './styles';
 
-
- import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
  const SignUp: React.FC = () => {
-   return <Container />
+   return(
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1}}
+        behavior={ Platform.OS === 'ios' ? 'padding' : undefined }
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1}}
+        >
+          <Container>
+            <Image style={styles.logoImg} source={logoImg} />
+            <Title>Crie sua conta</Title>
+
+            <Input name="text" icon="user" placeholder="Nome"/>
+            <Input name="email" icon="mail" placeholder="E-mail"/>
+            <Input name="password" icon="lock" placeholder="Senha"/>
+            <Input name="password" icon="home" placeholder="Filial de"/>
+
+            <Button
+              onPress={() => {}}
+            >
+              Cadastrar
+            </Button>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+            <BackToLogin
+              onPress={() => {}}
+            >
+            <Icon name="arrow-left" size={20} color="#A22C32" />
+              <BackToLoginText style={{ marginLeft: 20 }}>Voltar para tela de login</BackToLoginText>
+            </BackToLogin>
+    </>
+   );
  };
+
+ const styles = StyleSheet.create({
+  logoImg: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    marginTop: 100
+  },
+ });
 
  export default SignUp;
--- src/pages/SignUp/styles.tsx
+++ src/pages/SignUp/styles.tsx
@@ -1,3 +1,29 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+export const Title = styled.Text`
+  font-size: 22px;
+  color: #fff;
+`;
+export const BackToLogin = styled.TouchableOpacity`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  border-top-width: 1px;
+  border-color: #232129;
+  background: #0105;
+`;
+export const BackToLoginText = styled.Text`
+  color: #fff;
+  font-family: 'Roboto_500Medium';
+  font-size: 18px;
+`;
